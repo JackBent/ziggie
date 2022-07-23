b0199b12457efe602f2135d72c5f51da3cbc6bcc
Jack Bent <John_Leslie_Bent@outlook.com>
2022-07-23 19:16:46 -0400

    "map working"

--- ziggie/src/compenents/Map/Map.jsx
+++ ziggie/src/compenents/Map/Map.jsx
@@ -11,8 +11,24 @@ import useStyles from './styles';
 const Map = () => {
 
     const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coords = { lat : 0, lng: 0}
     return (
-        <h1>MAP</h1>)
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: 'AIzaSyB-9mK9avFE5-yPNs7OkeFhRPw-HolPTOA'}}
+                defaultCenter={coords}
+                center={coords}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}>
+                    
+
+            </GoogleMapReact>
+        </div>
+       )
 }
 
 export default Map;
